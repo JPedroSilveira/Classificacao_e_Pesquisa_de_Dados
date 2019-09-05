91c39fe16c69760d34de3f0ab001c9a465a7b8dc
João <jpedross1999@gmail.com>
2019-09-05 20:21:04 -0300

    Desafio 1

--- Sort Compare/main.py
+++ Sort Compare/main.py
@@ -4,7 +4,7 @@ import test
 
 def main():
     repeat = 10
-    jump = 100000
+    jump = 10000
     size_min = 1000
     size_max = 1001000
 
@@ -13,7 +13,6 @@ def main():
     sort_list = sort.get_sorts(['improved_quick_sort'])
 
     test.test_battery(repeat, size_min, size_max, jump, sort_list, arr_type)
-
     arr_type = 'E'
 
     sort_list = sort.get_sorts(['quick_sort', 'improved_quick_sort'])
@@ -25,6 +24,5 @@ def main():
     sort_list = sort.get_sorts(['quick_sort', 'improved_quick_sort'])
 
     test.test_battery(repeat, size_min, size_max, jump, sort_list, arr_type)
-
 
 main()
